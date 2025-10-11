db7eab7d9281509c3fc9502732c815e12dedc490
PozdeevaElizaveta <Tyuiu.PozdeevaEA@outlook.com>
2025-10-11 20:25:24 +0500

    Task3

---
+++ Tyuiu.PozdeevaEA.Sprint2.Task3.V29.Test/DataServiceTest.cs
@@ -0,0 +1,48 @@
+﻿using Tyuiu.PozdeevaEA.Sprint2.Task3.V29.Lib;
+namespace Tyuiu.PozdeevaEA.Sprint2.Task3.V29.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCondition1()
+        {
+            DataService ds = new DataService();
+            double x = 5;
+            double res = ds.Calculate(x);
+            double wait = 37.969;
+            Assert.AreEqual(wait, res);
+        }
+
+        [TestMethod]
+        public void ValidCondition2()
+        {
+            DataService ds = new DataService();
+            double x = 0;
+            double res = ds.Calculate(x);
+            double wait = 1;
+            Assert.AreEqual(wait, res);
+        }
+
+        [TestMethod]
+        public void ValidCondition3()
+        {
+            DataService ds = new DataService();
+            double x = -10;
+            double res = ds.Calculate(x);
+            double wait = 0.905;
+            Assert.AreEqual(wait, res);
+        }
+
+        [TestMethod]
+        public void ValidCondition4()
+        {
+            DataService ds = new DataService();
+            double x = -66;
+            double res = ds.Calculate(x);
+            double wait = -725.985;
+            Assert.AreEqual(wait, res);
+        }
+
+    }
+}
---
+++ Tyuiu.PozdeevaEA.Sprint2.Task3.V29/Program.cs
@@ -0,0 +1,39 @@
+﻿using Tyuiu.PozdeevaEA.Sprint2.Task3.V29.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #2 | Выполнил: Поздеева Е. А. | ПКТб-25-1";
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #2                                                               *");
+        Console.WriteLine("* Тема: Вложенные операторы if - else                                     *");
+        Console.WriteLine("* Задание #3                                                              *");
+        Console.WriteLine("* Вариант #29                                                             *");
+        Console.WriteLine("* Выполнил: Поздеева Елизавета Александровна | ПКТб-25-1                  *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Написать программу, которая вычисляет требуемое значение функции Y      *");
+        Console.WriteLine("* с использованием вложенных оператор if-else, где пользователь           *");
+        Console.WriteLine("* вводит значение переменной X с клавиатуры.                              *");
+        Console.WriteLine("* Округлить полученное значение до трех знаков после запятой.             *");
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine("Введите значение переменной X: ");
+        double x = Convert.ToDouble(Console.ReadLine());
+        double res = ds.Calculate(x);
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine("Значение функции = " + res);
+        Console.ReadKey();
+
+    }
+}
